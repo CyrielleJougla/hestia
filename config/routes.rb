7aca65f481b0d48130716393d0944a86426a8c18
Vincent Fiorini <vincent76e@gmail.com>
2020-12-01 12:44:19 +0100

    create routes, controller and views houses

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'houses/index'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'houses#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :house, only: [:index,:new, :create]
 end
