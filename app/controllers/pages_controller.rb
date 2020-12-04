a8fe190f50c50099087d234cf1b5bcc81f026523
Cyrielle Jougla <cyrielle.jougla1@gmail.com>
2020-12-04 12:17:46 +0100

    seeds et redirect home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def home
-    redirect_to house_path(current_user.house) if current_user.house
+    redirect_to house_path(current_user.habitant.house) if current_user.habitant
     redirect_to new_profile_path unless current_user.profile
   end
 
