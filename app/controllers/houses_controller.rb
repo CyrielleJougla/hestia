e4f94df158a143679bbc63ca41e9e63d4014bd6c
Vincent Fiorini <vincent76e@gmail.com>
2020-12-07 12:33:27 +0100

    le back de la progress bar est terminé (sa taille est progressive en fonction du total de points des taches) MAIS elle  à besoin d'être stylisée en front

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -22,6 +22,11 @@ class HousesController < ApplicationController
     @user = current_user
     @house = House.find(params[:id])
     @tasks = @house.tasks
+    @tot_tasks = Task.all
+    @total_points_tasks = 0
+    @tot_tasks.each do |task|
+      @total_points_tasks += task.points
+    end
   end
 
   private
