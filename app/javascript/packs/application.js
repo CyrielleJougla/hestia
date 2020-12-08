844823b78797ed07ef1203aef6d6216105048996
Cyrielle Jougla <cyrielle.jougla1@gmail.com>
2020-12-08 18:11:02 +0100

    enlever doublon dans app.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,7 +25,6 @@ require("channels")
 // External imports
 import "bootstrap";
 
-import { initScoreBar } from '../components/scorebar';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
