ed63128e8a956f1353148317248e1c4163216427
Cyrielle Jougla <cyrielle.jougla1@gmail.com>
2020-12-08 16:45:36 +0100

    reparé le js du form new task, et rajouté bouton fait sur la show task

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,17 +29,13 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { taskForm } from '../components/task-form';
 import { initSelect2 } from '../plugins/init_select2';
-
-// [...]
-
-
 import { initScoreBar } from '../components/scorebar';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  initSelect2();
   taskForm();
+  initSelect2();
   initScoreBar();
 });
 
