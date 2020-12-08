1794aabc743d4385e8f1fd39808d80a0fd68a550
Vincent Fiorini <vincent76e@gmail.com>
2020-12-08 16:16:50 +0100

    plus de conflits... CHAMPAGNE

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,21 +25,20 @@ require("channels")
 // External imports
 import "bootstrap";
 
+import { initScoreBar } from '../components/scorebar';
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-import { taskForm } from '../components/task-form';
-import { initSelect2 } from '../plugins/init_select2';
+// import { taskForm } from '../components/task-form';
+// import { initSelect2 } from '../plugins/init_select2';
 
 // [...]
-
-
-import { initScoreBar } from '../components/scorebar';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  initSelect2();
-  taskForm();
+  // initSelect2();
+  // taskForm();
   initScoreBar();
 });
 
